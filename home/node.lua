a528005ff602ad2ad2fda602cb2d69ff90efb54e
Christian Kellermann <ckeen@pestilenz.org>
2018-01-15 14:51:06 +0100

    home: remove animations, those are silly

--- home/node.lua
+++ home/node.lua
@@ -13,18 +13,11 @@ new_h = (new_w / w) * h
 x = (WIDTH - new_w) / 2
 y = (HEIGHT - new_h) / 5
 
--- angle
-a = 0
-
 function node.render()
     gl.clear(0, 0, 0, 1)
     font:write(370, 550, "K4CG InfoScreen", 60, 0, 0.5, 0, 1)
     font:write(290, 650, "please contribute:         /k4cg/infoscreen-v2", 40, 0, 0.5, 0, 1)
     gh:draw(530, 640, 600, 700)
 
-    gl.translate(WIDTH / 2, 0, 0)
-    gl.rotate(a, 0, 1, 0)
-    a = (a + 2) % 360
-
-    logo:draw(- WIDTH / 2 + x, y, new_w - WIDTH / 2, new_h)
+    logo:draw(0, 0, new_w , new_h)
 end
