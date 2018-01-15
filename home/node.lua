ce1642b6e032e39f4255fe0959117ad122be3bda
Christian Kellermann <ckeen@pestilenz.org>
2018-01-15 12:57:55 +0100

    home: change to K4CG branding

--- home/node.lua
+++ home/node.lua
@@ -2,25 +2,25 @@ gl.setup(1024, 768)
 
 gh = resource.load_image("Octocat.png")
 font = resource.load_font("miso-regular.ttf")
-logo = resource.load_image("rzl-logo.png")
+logo = resource.load_image("k4cg-logo.png")
 
 -- scale image
 w, h = logo:size()
 
-new_w = 0.9 * WIDTH
+new_w = 0.7 * WIDTH
 new_h = (new_w / w) * h
 
 x = (WIDTH - new_w) / 2
-y = (HEIGHT - new_h) / 2
+y = (HEIGHT - new_h) / 5
 
 -- angle
 a = 0
 
 function node.render()
-    gl.clear(1, 1, 1, 1)
-    font:write(370, 500, "InfoScreen v2", 60, 0, 0, 0, 1)
-    font:write(290, 600, "please contribute:         /infoscreen-v2", 40, 0, 0, 0, 1)
-    gh:draw(530, 590, 600, 650)
+    gl.clear(0, 0, 0, 1)
+    font:write(370, 550, "K4CG InfoScreen", 60, 0, 0.5, 0, 1)
+    font:write(290, 650, "please contribute:         /k4cg/infoscreen-v2", 40, 0, 0.5, 0, 1)
+    gh:draw(530, 640, 600, 700)
 
     gl.translate(WIDTH / 2, 0, 0)
     gl.rotate(a, 0, 1, 0)
