94365cc3618274766b0ea3f9b5259fc31ceeb435
Christian Kellermann <ckeen@pestilenz.org>
2018-01-15 11:50:22 +0100

    first prototype for VAG display

--- fahrplan/node.lua
+++ fahrplan/node.lua
@@ -1,32 +1,68 @@
-gl.setup(1024, 768)
+node.alias('fahrplan')
+gl.setup(1080,1920)
 
 font = resource.load_font("miso-regular.ttf")
-logo = resource.load_image("rnv-logo.png")
+--logo = resource.load_image("vag-logo.png")
 
 json = require "json"
 
-util.file_watch("fahrplan.json", function(content)
-    fahrplan = json.decode(content)
-    last_update = sys.now
-end)
+local current_time = N.current_time or "00:00"
+local current_seconds = N.current_seconds or 0
+
+util.data_mapper{
+    ["clock/set"] = function(time)
+        current_time = time
+        N.current_time = current_time
+--        print("new time " .. current_time)
+    end;
+    ["seconds/set"] = function(seconds)
+        current_seconds = tonumber(seconds)
+        N.current_seconds = current_seconds
+        node.render()
+    end;
+    }
+
+local fahrplan = N.fahrplan or {["Haltestelle"] = "", ["Abfahrten"] = {}}
 
 function node.render()
-    gl.clear(1, 1, 1, 1)
-    font:write(50, 40, "Fahrplan", 90, 0,0,0,1)
-    logo:draw(800, 60, 930, 110)
+    gl.clear(0, 0, 0, 1)
+    font:write(50, 30, "Abfahrten ab", 35, 0.5,0.6,0.3,1)
+    font:write(50, 60, fahrplan.Haltestelle, 70, 0.8,0.8,0.3,1)
+  --  logo:draw(800, 60, 1000, 120)
 
-    font:write(50, 140, "Uhrzeit", 50, 0,0,0,1)
-    font:write(300, 140, "Linie", 50, 0,0,0,1)
-    font:write(400, 140, "Richtung", 50, 0,0,0,1)
-    font:write(750, 140, "Verspätung", 50, 0,0,0,1)
-    for idx, fahrt in ipairs(fahrplan) do
-        font:write(50, 150 + 50 * idx, fahrt.uhrzeit, 40, 0,0,0,1)
-        font:write(330, 150 + 50 * idx, fahrt.linie, 40, 0,0,0,1)
-        font:write(400, 150 + 50 * idx, fahrt.ziel, 40, 0,0,0,1)
-        if 0+fahrt.verspaetung > 0 then
-            font:write(830, 150 + 50 * idx, "+" .. fahrt.verspaetung, 40, 1,0,0,1)
+    font:write(50, 140, "Abfahrt", 50,  0.5,0.6,0.3,1)
+    font:write(300, 140, "Linie", 50,  0.5,0.6,0.3,1)
+    font:write(400, 140, "Richtung", 50, 0.5,0.6,0.3,1)
+    font:write(750, 140, "Verspätung", 50, 0.5,0.6,0.3,1)
+    idx = 1
+    for k,fahrt in ipairs(fahrplan.Abfahrten) do
+        departure = math.floor((fahrt.AbfahrtszeitSoll - current_seconds) / 60)
+        if departure == 0 then
+           departure = "jetzt"
         else
-            font:write(830, 150 + 50 * idx, "+" .. fahrt.verspaetung, 40, 0,0,0,1)
+           departure = "in " .. departure  .. " min."
+        end
+        if (fahrt.AbfahrtszeitSoll > current_seconds) then
+                font:write(50, 150 + 50 * idx, departure, 40, 0.8,0.8,0.3,1)
+                font:write(330, 150 + 50 * idx, fahrt.Linienname, 40,  0.8,0.8,0.3,1)
+                font:write(400, 150 + 50 * idx, fahrt.Richtungstext, 40, 0.8,0.8,0.3,1)
+                if fahrt.Verspaetung > 0 then
+                      font:write(830, 150 + 50 * idx, "+" .. fahrt.Verspaetung, 40, 0.5,0.4,0.8,1)
+                else
+                      font:write(830, 150 + 50 * idx, "0", 40, 0.8,0.8,0.3,1)
+                end
+                idx = idx + 1
+                if idx > 30 then
+                   break
+                end
         end
     end
+    font:write(WIDTH - 150, HEIGHT - 50, current_time, 50, 0.8,0.8,0.3,1)
 end
+
+node.event("input", function(line, client)
+    fahrplan = json.decode(line)
+    N.fahrplan = fahrplan
+    print("departures update")
+    node.render()
+end)
---
+++ fahrplan/vag.scm
@@ -0,0 +1,48 @@
+(use medea http-client miscmacros uri-common udp tcp)
+
+(determine-proxy
+ (lambda (u) (uri-reference "http://proxy:3128/")))
+
+(define (cleanup-times entry)
+  (let* ((t-ist (string->time (alist-ref 'AbfahrtszeitIst entry) "%Y-%m-%dT%T%z"))
+        (t-soll (string->time (alist-ref 'AbfahrtszeitSoll entry) "%Y-%m-%dT%T%z"))
+        (verspaetung (modulo (- (local-time->seconds t-ist) (local-time->seconds t-soll)) 60)))
+    (cons (cons 'Verspaetung verspaetung)
+          (alist-update 'AbfahrtszeitIst (local-time->seconds t-ist)
+                  (alist-update 'AbfahrtszeitSoll (local-time->seconds t-soll) entry)))))
+
+(define (get-departures station-id)
+      (let* ((res (with-input-from-request
+                   (sprintf "http://start.vag.de/dm/api/v1/abfahrten/VGN/~a?product=Bus%2CTram%2CUBahn&timespan=30&limitcount=50" station-id)
+                   #f read-json))
+             (departures (alist-ref 'Abfahrten res)))
+         (cons (cons 'Haltestelle (alist-ref 'Haltestellenname res))
+              `((Abfahrten . ,(list->vector (map cleanup-times (vector->list departures))))))))
+
+
+
+(define (send-time)
+  (let ((s (udp-open-socket)))
+    (udp-bind! s #f 0)
+    (udp-connect! s "127.0.0.1" 4444)
+    (udp-send s (sprintf "fahrplan/seconds/set:~a\n" (current-seconds)))
+    (udp-send s (sprintf "fahrplan/clock/set:~a\n" (time->string (seconds->local-time (current-seconds)) "%T")))
+    (udp-close-socket s)))
+
+  (let loop ()
+    (send-time)
+    (let ((deps (get-departures 1103)))
+      (define-values (in out) (tcp-connect "localhost" 4444))
+      (print (read-line in))
+      (display "fahrplan" out)(newline out)
+      (print (read-line in))
+      (write-json deps out)
+      (newline out)
+      (close-input-port in)
+      (close-output-port out)
+      (repeat 60
+              (send-time)
+              (sleep 1))
+      (loop)))
+
+
